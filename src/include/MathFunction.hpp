ece09e50fbdcec4a7cf26e8e6fc3638cf165179b
gzfrozen <gzliuxiaochen@gmail.com>
2020-07-10 01:24:52 +0900

    simplified MathFunction.

--- src/include/MathFunction.hpp
+++ src/include/MathFunction.hpp
@@ -5,13 +5,8 @@
 /* change spherical vector into normalized xyz vector*/
 __host__ __device__ inline vec3f sphere_to_normal(const vec3f &sphere_direction)
 {
-    float theta = sphere_direction.y;
-    float phi = fmod(sphere_direction.z, 2 * M_PI);
-    if (phi > M_PI)
-    {
-        theta = theta + M_PI;
-        phi = 2 * M_PI - phi;
-    }
+    const float &theta = sphere_direction.y;
+    const float &phi = sphere_direction.z;
     return vec3f(cos(theta) * sin(phi),
                  sin(theta) * sin(phi),
                  cos(phi));
@@ -24,10 +19,7 @@ __host__ __device__ inline vec3f normal_to_sphere(const vec3f &xyz_direction)
     float phi = acos(xyz_direction.z / length(xyz_direction));
     if (xyz_direction.x < 0)
     {
-        if (xyz_direction.y > 0)
-            theta += M_PI;
-        else
-            theta -= M_PI;
+        theta = (xyz_direction.y > 0) ? theta + M_PI : theta - M_PI;
     }
     return vec3f(0.f, theta, phi);
 }
